d379c972db5ab4a42adf46590970badec00cb5db
Doug <git@pixlwave.uk>
2017-01-31 21:30:11 +0000

    Update directly based upon HapInAVF Test App code. Still crashes with a zombie frame.

--- Hap-AVF-Syphon/ViewController.swift
+++ Hap-AVF-Syphon/ViewController.swift
@@ -11,9 +11,7 @@ class ViewController: NSViewController {
     
     var player: AVPlayer?
     var hapOutput = AVPlayerItemHapDXTOutput()
-    var emptyFrame: HapDecoderFrame?
-    var bufferArray: [VVBuffer]?
-    var dxtFrame: HapDecoderFrame?
+    var lastRenderedBuffer: VVBuffer?
     var size = NSSize(width: 640, height: 360)
     var syphonServer: SyphonServer?
     
@@ -37,36 +35,34 @@ class ViewController: NSViewController {
         
         hapOutput.suppressesPlayerRendering = true
         
-        //  make the "alloc frame" block: this takes a CMSampleBufferRef and returns a HapDecoderFrame that has been configured and is ready to be decompressed into
-        hapOutput.setAllocFrameBlock { (decompressMe) -> HapDecoderFrame? in
-            //  make an empty decoder frame from the buffer (the basic fields describing the data properties of the DXT frame are populated, but no memory is allocated to decompress the DXT into)
-            if let emptyFrame = HapDecoderFrame(emptyWithHapSampleBuffer: decompressMe),
-                //  make a CPU-backed/tex range VVBuffer for each plane in the decoder frame
-                let bufferArray = self.globalBufferPool.createBuffers(for: emptyFrame) as? [VVBuffer] {
-                //  populate the hap decoder frame i'll be returning with the CPU-based memory from the buffers, and ensure that the decoder will retain the buffers (this has to be done for each plane in the frame)
-                for (i, buffer) in bufferArray.enumerated() {
-                    emptyFrame.dxtDatas[i] = buffer.cpuBackingPtr()
-                    emptyFrame.dxtDataSizes[i] = Int(VVBufferDescriptorCalculateCPUBackingForSize(buffer.descriptorPtr(), buffer.backingSize))
+        //	make the decoder frame alocator block: i want to create a frame that will decode into a texture range- RAM that is mapped directly to VRAM and uploads via DMA
+        hapOutput.setAllocFrameBlock { decompressMe -> HapDecoderFrame? in
+            let emptyFrame: HapDecoderFrame?
+            if decompressMe != nil {
+                //	make an empty decoder frame from the buffer (the basic fields describing the data properties of the DXT frame are populated, but no memory is allocated to decompress the DXT into)
+                emptyFrame = HapDecoderFrame(emptyWithHapSampleBuffer: decompressMe)
+                //	make a CPU-backed/tex range VVBuffers for each plane in the decoder frame
+                if let bufferArray = self.globalBufferPool.createBuffers(for: emptyFrame) as? [VVBuffer] {
+                    //	populate the hap decoder frame i'll be returning with the CPU-based memory from the buffer, and ensure that the decoder will retain the buffers (this has to be done for each plane in the frame)
+                    for (i, buffer) in bufferArray.enumerated() {
+                        emptyFrame?.dxtDatas[i] = buffer.cpuBackingPtr()
+                        emptyFrame?.dxtDataSizes[i] = Int(VVBufferDescriptorCalculateCPUBackingForSize(buffer.descriptorPtr(), buffer.backingSize))
+                    }
+                    //	add the array of buffers to the frame's userInfo- we want the frame to retain the array of buffers...
+                    emptyFrame?.userInfo = bufferArray
+                    
+                    return emptyFrame
                 }
-                //  add the array of buffers to the frame's userInfo- we want the frame to retain the array of buffers...
-                emptyFrame.userInfo = bufferArray
-                self.emptyFrame = emptyFrame
-                return emptyFrame
             }
             return nil
         }
         
-        //  make the post-decode frame block: tell the buffers from the decoded frame that their backing has been updated
-        hapOutput.setPostDecode { (decodedFrame) in
-            if let buffers = decodedFrame?.userInfo as? [VVBuffer] {
-                print("Number of bufs: \(buffers.count)")
-                for buffer in buffers {
-                    VVBufferPool.pushTexRangeBufferRAMtoVRAM(buffer, usingContext: self.context?.cglContextObj)
-                }
-                //  ...at this point, the VVBuffer instances in "buffers" have the images that you want to work with- do whatever you want with 'em!
-                self.bufferArray = buffers
-            }
+        //	make the post decode block: after decoding, i want to upload the DXT data to a GL texture via DMA, on the decode thread
+        let tmpBlock: AVFHapDXTPostDecodeBlock = { [weak self] decodedFrame in
+            self?.finishDecodingHapFrame(decodedFrame)
         }
+        
+        hapOutput.setPostDecode(tmpBlock)
         
         player = AVPlayer(url: Bundle.main.url(forResource: "video_hap", withExtension: "mov")!)
         player?.actionAtItemEnd = .none
@@ -75,16 +71,33 @@ class ViewController: NSViewController {
     }
     
     func screenRefresh() {
-        let outputTime = hapOutput.itemTime(forHostTime: CACurrentMediaTime())
-        dxtFrame = hapOutput.allocFrame(for: outputTime)
-        if let buffer = bufferArray?.first {
-            isfScene.setFilterInputImageBuffer(buffer)
-            if let isfOutput = isfScene.allocAndRender(toBufferSized: size) {
-                syphonServer?.publishFrameTexture(isfOutput.name, textureTarget: isfOutput.target, imageRegion: isfOutput.srcRect, textureDimensions: isfOutput.size, flipped: isfOutput.flipped)
+        objc_sync_enter(self)       // closest to objc @synchronised self?
+            let frameTime = hapOutput.itemTime(forMachAbsoluteTime: Int64(mach_absolute_time()))
+            let dxtFrame = hapOutput.allocFrameClosest(to: frameTime)
+        
+            if let frameBuffers = dxtFrame?.userInfo as? [VVBuffer], let buffer = frameBuffers.first {
+                
+                isfScene.setFilterInputImageBuffer(buffer)      // just a placeholder for swizzle scenes
+                
+                if let isfOutput = isfScene.allocAndRender(toBufferSized: size) {
+                    syphonServer?.publishFrameTexture(isfOutput.name, textureTarget: isfOutput.target, imageRegion: isfOutput.srcRect, textureDimensions: isfOutput.size, flipped: isfOutput.flipped)
+                    lastRenderedBuffer = isfOutput
+                }
             }
-        }
+        objc_sync_exit(self)
         
         globalBufferPool.housekeeping()
+    }
+    
+    func finishDecodingHapFrame(_ decodedFrame: HapDecoderFrame?) {
+        //	this method is called from a dispatch queue owned by the AVPlayerItemHapDXTOutput- this is important, because of the operating's restrictions on running GL contexts: a GL context may be used only by one thread at a time.  since this method will potentially be called simultaneously from multiple threads, we need to synchronized access to the GL context that this method uses to upload texture data.  we're doing this with a simple @synchronized here, but a pool of contexts would also be effective.
+        if let newBuffers = decodedFrame?.userInfo as? [VVBuffer] {
+            objc_sync_enter(self)       // closest to objc @synchronised self?
+                for newBuffer in newBuffers {
+                    VVBufferPool.pushTexRangeBufferRAMtoVRAM(newBuffer, usingContext: context?.cglContextObj)
+                }
+            objc_sync_exit(self)
+        }
     }
     
     @IBAction func rewind(_ sender: AnyObject) {
